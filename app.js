1067ade4fd417ec9332fcc06a1dc6f4ffbc1e70d
haapamakituomas <115136792+haapamakituomas@users.noreply.github.com>
2023-08-21 13:36:57 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part, index) => (
+        <Part key={index} part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <p>Number of exercises {total}</p>;
+};
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 },
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+};
+
+export default App;
